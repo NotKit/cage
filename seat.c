swaywm/sway

input: Clean up input_method_relay in seat_destoy()
This fixes a crash that happens when input_method_new or text_method_new
events are emitted after the seat has been freed.

sway/input/seat.c
@@ -51,6 +51,7 @@ void seat_destroy(struct sway_seat *seat) {
 	wl_list_for_each_safe(seat_device, next, &seat->devices, link) {
 		seat_device_destroy(seat_device);
 	}
+	sway_input_method_relay_finish(&seat->im_relay);
 	sway_cursor_destroy(seat->cursor);
 	wl_list_remove(&seat->new_node.link);
 	wl_list_remove(&seat->request_start_drag.link);

sway/input/text_input.c
@@ -263,6 +263,11 @@ void sway_input_method_relay_init(struct sway_seat *seat,
 		&relay->input_method_new);
 }
 
+void sway_input_method_relay_finish(struct sway_input_method_relay *relay) {
+	wl_list_remove(&relay->input_method_new.link);
+	wl_list_remove(&relay->text_input_new.link);
+}
+
 void sway_input_method_relay_set_focus(struct sway_input_method_relay *relay,
 		struct wlr_surface *surface) {
 	struct sway_text_input *text_input;

include/sway/input/text_input.h
@@ -53,6 +53,8 @@ struct sway_text_input {
 void sway_input_method_relay_init(struct sway_seat *seat,
 	struct sway_input_method_relay *relay);
 
+void sway_input_method_relay_finish(struct sway_input_method_relay *relay);
+
 // Updates currently focused surface. Surface must belong to the same seat.
 void sway_input_method_relay_set_focus(struct sway_input_method_relay *relay,
 	struct wlr_surface *surface);

NotKit/cage

seat.c
@@ -829,6 +829,7 @@ seat_destroy(struct cg_seat *seat)
 		return;
 	}
 
+	cg_input_method_relay_finish(&seat->im_relay);
 	wl_list_remove(&seat->request_start_drag.link);
 	wl_list_remove(&seat->start_drag.link);
 

text_input.c
@@ -263,6 +263,11 @@ void cg_input_method_relay_init(struct cg_seat *seat,
 		&relay->input_method_new);
 }
 
+void cg_input_method_relay_finish(struct cg_input_method_relay *relay) {
+	wl_list_remove(&relay->input_method_new.link);
+	wl_list_remove(&relay->text_input_new.link);
+}
+
 void cg_input_method_relay_set_focus(struct cg_input_method_relay *relay,
 		struct wlr_surface *surface) {
 	struct cg_text_input *text_input;

text_input.h
@@ -53,6 +53,8 @@ struct cg_text_input {
 void cg_input_method_relay_init(struct cg_seat *seat,
 	struct cg_input_method_relay *relay);
 
+void cg_input_method_relay_finish(struct cg_input_method_relay *relay);
+
 // Updates currently focused surface. Surface must belong to the same seat.
 void cg_input_method_relay_set_focus(struct cg_input_method_relay *relay,
 	struct wlr_surface *surface);