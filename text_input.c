swaywm/sway

input-method: avoid and log unneeded set_focus

sway/input/text_input.c
@@ -297,6 +297,9 @@ void sway_input_method_relay_set_focus(struct sway_input_method_relay *relay,
 			if (surface != text_input->input->focused_surface) {
 				relay_disable_text_input(relay, text_input);
 				wlr_text_input_v3_send_leave(text_input->input);
+			} else {
+				sway_log(SWAY_DEBUG, "IM relay set_focus already focused");
+				continue;
 			}
 		}
 

NotKit/cage

text_input.c
@@ -298,6 +298,9 @@ void cg_input_method_relay_set_focus(struct cg_input_method_relay *relay,
 			if (surface != text_input->input->focused_surface) {
 				relay_disable_text_input(relay, text_input);
 				wlr_text_input_v3_send_leave(text_input->input);
+			} else {
+				wlr_log(WLR_DEBUG, "IM relay set_focus already focused");
+				continue;
 			}
 		}
 